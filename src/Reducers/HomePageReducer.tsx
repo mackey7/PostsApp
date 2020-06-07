bf58c20170126e3a2f531cf47a4a2ccdc7c60f25
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-06-07 20:01:57 +0200

    crate HomePage reduer

---
+++ src/Reducers/HomePageReducer.tsx
@@ -0,0 +1,19 @@
+import { Reducer } from 'redux'
+import { FETCH_USERS_SUCCESS } from '../Actions/actions_types/index'
+
+export interface IUserState {
+    users: string[];
+}
+
+const initial = {
+    users: []
+}
+
+export const HomePageReducer: Reducer<IUserState> = (state = initial, actions) => {
+    switch (actions.type) {
+        case FETCH_USERS_SUCCESS:
+            return { users: actions.payload }
+        default:
+            return state
+    }
+}
