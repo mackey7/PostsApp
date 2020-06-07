2b0f36e8d56b2e1f5047ce4575c4b01e0439934d
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-06-07 20:33:00 +0200

    connect homePage reducer with index reducer

---
+++ src/Reducers/index.tsx
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux'
+import { HomePageReducer } from './HomePageReducer'
+
+const rootReducer = combineReducers({
+    HomePageReducer
+})
+
+export default rootReducer
