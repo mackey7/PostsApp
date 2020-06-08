99b99134bcb1545df1f710c3b62bb44463e8dedf
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-06-08 19:04:53 +0200

    add loader to Home Component

--- src/Components/HomeComponent/index.tsx
+++ src/Components/HomeComponent/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { HomeWrapper } from '../../Helpers/Styles/HomeComponent/HomeComponent'
+import { CenterElement, Loader } from '../../Helpers/Styles/GeneralStyles'
+
 
 export const HomeComponent = ({ UserData: any }) => {
     return (
         <HomeWrapper>
-
+            <CenterElement>
+                <Loader>  </Loader>
+            </CenterElement>
         </HomeWrapper>
     )
 
