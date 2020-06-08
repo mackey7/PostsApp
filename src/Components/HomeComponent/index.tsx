438adf00e51e08f4e97fa37900e6dcd37d0ccc28
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-06-08 19:21:53 +0200

    create condition render  in home component

--- src/Components/HomeComponent/index.tsx
+++ src/Components/HomeComponent/index.tsx
@@ -3,14 +3,25 @@ import { HomeWrapper } from '../../Helpers/Styles/HomeComponent/HomeComponent'
 import { CenterElement, Loader } from '../../Helpers/Styles/GeneralStyles'
 
 
-export const HomeComponent = ({ UserData: any }) => {
-    return (
-        <HomeWrapper>
-            <CenterElement>
-                <Loader>  </Loader>
-            </CenterElement>
-        </HomeWrapper>
-    )
+export const HomeComponent = ({ UserData }: any) => {
+    if (UserData.length > 0) {
+        const mapUsers = UserData.map((data: any) => {
+            return (<div></div>)
+        })
+        return (
+            <HomeWrapper >
+                {mapUsers}
+            </HomeWrapper>
+        )
 
+    } else {
+        return (
+            <HomeWrapper>
+                <CenterElement>
+                    <Loader>  </Loader>
+                </CenterElement>
+            </HomeWrapper>
+        )
+    }
 
 }
