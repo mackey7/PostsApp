7e811616d41e2e271b0084ec08aaed2d56484cf0
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-06-08 21:16:08 +0200

    create UserCardWrapper style tag

--- src/Helpers/Styles/HomeComponent/HomeComponent.js
+++ src/Helpers/Styles/HomeComponent/HomeComponent.js
@@ -7,3 +7,14 @@ width:80%;
 display:flex;
 flex-wrap:wrap;
 `
+
+export const UserCardWrapper = Styled.article`
+max-width:33%;
+width:200px;
+display:flex;
+flex-direction:column;
+border:3px solid ${BLACK};
+min-height:400px;
+margin:20px;
+padding:30px;
+`
