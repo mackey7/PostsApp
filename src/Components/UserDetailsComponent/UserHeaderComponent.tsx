81cc2b4e6b585b5affd11d6bd3aa140a77ade5e7
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-06-09 21:34:53 +0200

    create addBtn tag with style

--- src/Components/UserDetailsComponent/UserHeaderComponent.tsx
+++ src/Components/UserDetailsComponent/UserHeaderComponent.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { ComponentWrapper, BtnBack, Title } from '../../Helpers/Styles/UserDetailsComponent/UserHeaderComponent'
+import { ComponentWrapper, BtnBack, Title, AddBtn } from '../../Helpers/Styles/UserDetailsComponent/UserHeaderComponent'
 
 export const UserHeaderComponent = () => {
     return (
         <ComponentWrapper>
             <BtnBack to={"/"} className="fas fa-arrow-left"><span>Back</span></BtnBack>
             <Title > Title</Title>
+            <AddBtn className="fas fa-plus"></AddBtn>
         </ComponentWrapper>
     )
 }
--- src/Helpers/Styles/UserDetailsComponent/UserHeaderComponent.js
+++ src/Helpers/Styles/UserDetailsComponent/UserHeaderComponent.js
@@ -2,6 +2,8 @@ import Styled from 'styled-components'
 import { Link } from 'react-router-dom'
 const BLUE = "#075394"
 const BLACK = "black"
+const WHITE = "white"
+
 
 export const ComponentWrapper = Styled.section`
 display:flex;
@@ -35,4 +37,18 @@ span{
 `
 export const Title = Styled.div`
 font-weight:700;
+`
+
+
+export const AddBtn = Styled.i`
+background:${BLUE};
+color:${WHITE};
+width:30px;
+height:30px;
+border-radius:30px;
+border:3px solid ${BLACK};
+display:flex;
+justify-content:center;
+align-items:center;
+cursor:pointer;
 `
