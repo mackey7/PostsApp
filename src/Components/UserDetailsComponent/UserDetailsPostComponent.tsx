bb37956facb1f04d3610124432abf7fcb48364c4
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-06-09 22:26:13 +0200

    crete link to post details

--- src/Components/UserDetailsComponent/UserDetailsPostComponent.tsx
+++ src/Components/UserDetailsComponent/UserDetailsPostComponent.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { ComponentWrapper, Btn, TitlePost } from '../../Helpers/Styles/UserDetailsComponent/UserDetailsTaskComponent'
+import { Link } from "react-router-dom";
 
 export const UserDetailsPostComponent = ({ data }: any) => {
     const { id, title, userId } = data
     return (
         <ComponentWrapper>
             <Btn className="far fa-trash-alt"></Btn>
-            <TitlePost data-testid="title" >{title}</TitlePost >
+            <TitlePost >{title}</TitlePost >
+            <Link to={`/user/${userId}/${id}`}><Btn className="fas fa-chevron-right"></Btn></Link>
         </ComponentWrapper>
     )
 }
