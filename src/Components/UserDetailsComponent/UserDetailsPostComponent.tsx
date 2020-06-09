679b78208791f122bc0e8a748ac7b7acece3d966
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-06-09 22:30:26 +0200

    add onClick width delete props

--- src/Components/UserDetailsComponent/UserDetailsPostComponent.tsx
+++ src/Components/UserDetailsComponent/UserDetailsPostComponent.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 import { ComponentWrapper, Btn, TitlePost } from '../../Helpers/Styles/UserDetailsComponent/UserDetailsTaskComponent'
 import { Link } from "react-router-dom";
 
-export const UserDetailsPostComponent = ({ data }: any) => {
+export const UserDetailsPostComponent = ({ data, deletePostFn }: any) => {
     const { id, title, userId } = data
+
     return (
         <ComponentWrapper>
-            <Btn className="far fa-trash-alt"></Btn>
+            <Btn className="far fa-trash-alt" onClick={() => deletePostFn(id)} ></Btn>
             <TitlePost >{title}</TitlePost >
             <Link to={`/user/${userId}/${id}`}><Btn className="fas fa-chevron-right"></Btn></Link>
         </ComponentWrapper>
