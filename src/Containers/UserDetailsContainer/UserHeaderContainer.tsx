398f7d4c494f91017cd10c1b747c0f3bc1b7e2d0
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-06-09 23:01:04 +0200

    create dispatch with fetchUser

--- src/Containers/UserDetailsContainer/UserHeaderContainer.tsx
+++ src/Containers/UserDetailsContainer/UserHeaderContainer.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from 'react-redux'
 import { UserHeaderComponent } from '../../Components/UserDetailsComponent/UserHeaderComponent'
+import { fetchUser } from '../../Actions/UserDetailsActions'
+
+
 export const UserHeaderContainer = () => {
-
-
+    let { id } = useParams(); //  user id, which is able in location
+    const dispatch = useDispatch();
+    // fetch data from store
+    useEffect(() => {
+        async function fetchData() {
+            await dispatch(fetchUser(id));//fetch user name to header
+        }
+        fetchData();
+    }, []);
+    const user = useSelector((state: any) => state.user.user)
     return (
         <React.Fragment>
             <UserHeaderComponent />
