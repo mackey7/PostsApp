42b23538bdd8bc471df2e5029052ab73f60fe713
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-06-09 20:47:54 +0200

    add types for map in HomeComponent

--- src/Components/HomeComponent/index.tsx
+++ src/Components/HomeComponent/index.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 import { HomeWrapper } from '../../Helpers/Styles/HomeComponent/HomeComponent'
 import { CenterElement, Loader } from '../../Helpers/Styles/GeneralStyles'
 import { UserCardComponent } from './UserCardComponent'
-import { UsersTypesComponent } from '../../Helpers/Types'
+import { UsersTypesComponent, userDataTypes } from '../../Helpers/Types'
 export const HomeComponent = ({ UserData }: UsersTypesComponent) => {
     if (UserData.length > 0) {
-        const mapUsers = UserData.map((data: any) => {
+        const mapUsers = UserData.map((data: userDataTypes) => {
             return (<UserCardComponent key={data.id} userData={data} />)
         })
         return (
