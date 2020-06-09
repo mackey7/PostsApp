3975a70db3f23c8e81268ab24a701d9dd4fdba1b
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-06-09 21:41:57 +0200

    create fetchUserPostsSuccess action

---
+++ src/Actions/UserDetailsActions.tsx
@@ -0,0 +1,22 @@
+import Axios from 'axios'
+import { FETCH_USER_POSTS_SUCCESS } from './actions_types/index'
+import { API_URL } from '../Helpers/ENV'
+
+export const fetchUserPostsSuccess = (payload: any) => {
+    return {
+        type: FETCH_USER_POSTS_SUCCESS,
+        payload
+    }
+}
+
+export const fetchUserPosts = (id: any) => {
+    return (dispatch: any) => {
+        return Axios.get(`${API_URL}/posts?userId=${id}`)
+            .then(response => {
+                dispatch(fetchUserPostsSuccess(response.data))
+            })
+            .catch(error => {
+                throw error
+            });
+    }
+}
--- src/Actions/actions_types/index.tsx
+++ src/Actions/actions_types/index.tsx
@@ -1,1 +1,2 @@
 export const FETCH_USERS_SUCCESS: string = "FETCH_USER_SUCCESS";
+export const FETCH_USER_POSTS_SUCCESS: string = "FETCH_USER_POSTS_SUCCESS"
