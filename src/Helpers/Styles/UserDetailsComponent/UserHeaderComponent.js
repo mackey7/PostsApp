0c49a9e4e6e346365e574719e9ddb08f663a3de3
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-06-09 20:58:33 +0200

    create ComponentWrapper style tag

---
+++ src/Helpers/Styles/UserDetailsComponent/UserHeaderComponent.js
@@ -0,0 +1,19 @@
+import Styled from 'styled-components'
+
+const BLACK = "black"
+
+export const ComponentWrapper = Styled.section`
+display:flex;
+margin:0 auto;
+width:84%;
+display:flex;
+flex-wrap:nowrap;
+justify-content:space-between;
+align-items:center;
+position:relative;
+
+padding:30px 10px 10px 10px;
+color:${BLACK};
+font-weight:500;
+font-size:24px;
+`
