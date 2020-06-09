277c39b8906e10dac6ac23e81180944fbad6998a
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-06-09 21:09:27 +0200

    add UserHeaderComponent to UserHeaderContainer

--- src/Containers/UserDetailsContainer/UserHeaderContainer.tsx
+++ src/Containers/UserDetailsContainer/UserHeaderContainer.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import { UserHeaderComponent } from '../../Components/UserDetailsComponent/UserHeaderComponent'
 export const UserHeaderContainer = () => {
 
 
     return (
-        <div>
-            UserHeaderContainer
-        </div>
+        <React.Fragment>
+            <UserHeaderComponent />
+        </React.Fragment>
     )
 }
