6bd40af1353f538d2ba641acbe5f602b9c9ae9b5
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-06-10 09:33:13 +0200

    create style for wrapper

--- src/Helpers/Styles/Modal/ModalAdd.js
+++ src/Helpers/Styles/Modal/ModalAdd.js
@@ -1,9 +1,16 @@
 import Styled from 'styled-components'
 
 export const Wrapper = Styled.section`
-
-
-
+position: absolute;
+display:flex;
+justify-content:center;
+align-items:center;
+width:100vw;
+height:100vh;
+background:rgb(0, 0, 0, 0.5);
+z-index: 999;
+left: 0;
+top: 0;
 `
 
 export const Content = Styled.section``
