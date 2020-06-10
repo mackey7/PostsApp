63df87af330a84c7aac0aacac6d5fd70bafa9502
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-06-10 09:26:45 +0200

    create form

--- src/Components/UserDetailsComponent/AddPostComponent.tsx
+++ src/Components/UserDetailsComponent/AddPostComponent.tsx
@@ -1,7 +1,28 @@
 import React from 'react'
-
+import { Wrapper, Content, Row, Label, Input, TextArea, BtnContainer, SaveBtn, CancelBtn, Form } from '../../Helpers/Styles/Modal/ModalAdd'
 export const AddPostComponent = () => {
     return (
-        <div> AddPostComponent</div>
+        <Wrapper>
+            <Content>
+                <h5>Add post</h5>
+                <Form>
+                    <h2> Add post</h2>
+                    <Row>
+                        <Label htmlFor="title">Title</Label>
+                        <Input type="text" name="title" />
+                    </Row>
+                    <Row>
+                        <Label htmlFor="body" >Body</Label>
+                        <TextArea name="body" ></TextArea>
+                    </Row>
+
+                </Form>
+                <BtnContainer>
+                    <CancelBtn type="button" value="Cancel" />
+                    <SaveBtn type="submit" value="Save" />
+                </BtnContainer>
+            </Content>
+
+        </Wrapper>
     )
 }
---
+++ src/Helpers/Styles/Modal/ModalAdd.js
@@ -0,0 +1,35 @@
+import Styled from 'styled-components'
+
+export const Wrapper = Styled.section`
+
+
+
+`
+
+export const Content = Styled.section``
+
+export const Row = Styled.div`
+
+`
+export const Input = Styled.input`
+
+`
+export const Label = Styled.label`
+p
+`
+export const TextArea = Styled.input`
+
+`
+export const BtnContainer = Styled.div`
+
+`
+export const CancelBtn = Styled.input`
+
+`
+
+export const SaveBtn = Styled.input`
+`
+
+export const Form = Styled.form`
+
+`
