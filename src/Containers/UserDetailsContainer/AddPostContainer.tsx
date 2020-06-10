861e8dfb1a660fbc905a067c967b047c6d52f60d
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-06-10 09:05:29 +0200

    create AddPostContainer and add to UserDetailsPage

---
+++ src/Containers/UserDetailsContainer/AddPostContainer.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const AddPostContainer = () => {
+    return (
+        <div>AddPostContainer</div>
+    )
+}
--- src/Pages/UserDetailsPage.tsx
+++ src/Pages/UserDetailsPage.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { UserHeaderContainer } from '../Containers/UserDetailsContainer/UserHeaderContainer'
 import { UserDetailsPostContainer } from '../Containers/UserDetailsContainer/UserDetailsPostContainer'
+import { AddPostContainer } from '../Containers/UserDetailsContainer/AddPostContainer'
 export const UserDetailsPage = () => {
     return (
         <React.Fragment>
+            <AddPostContainer />
             <UserHeaderContainer />
             <UserDetailsPostContainer />
         </React.Fragment>
