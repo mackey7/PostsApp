c0a6ee689bdade93fe4ee9e09bf06cbdefbd21fa
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-06-10 09:15:41 +0200

    add conditional render AddPostComponent

--- src/Containers/UserDetailsContainer/AddPostContainer.tsx
+++ src/Containers/UserDetailsContainer/AddPostContainer.tsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from 'react-redux'
+import { switchAddPostModal } from '../../Actions/UserDetailsActions'
 import { AddPostComponent } from '../../Components/UserDetailsComponent/AddPostComponent'
+
+
 export const AddPostContainer = () => {
+    const dispatch = useDispatch();
+    let { id } = useParams(); //  user id, which is able in location
+    const OpenModal = useSelector((state: any) => state.AddPostModalOpen.AddPostModalOpen)
+    const showAddPostFn = () => {
+        OpenModal ? dispatch(switchAddPostModal(false)) : dispatch(switchAddPostModal(true))
+    }
+    const renderAddPost = OpenModal ? <AddPostComponent /> : null
     return (
-        < AddPostComponent />
+        <React.Fragment>
+            {renderAddPost}
+        </React.Fragment>
     )
 }
