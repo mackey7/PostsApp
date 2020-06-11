c34586f4977ee1000b757c27a771291997797b3f
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-06-11 22:28:44 +0200

    fetch userName from api

--- src/Components/PostDetailsComponent/PostDetailsHeaderComponent.tsx
+++ src/Components/PostDetailsComponent/PostDetailsHeaderComponent.tsx
@@ -3,12 +3,12 @@ import { useParams } from "react-router-dom";
 import { BtnBack, Title, ComponentWrapper } from '../../Helpers/Styles/UserDetailsComponent/UserHeaderComponent'
 
 
-export const PostDetailsHeaderComponent = () => {
+export const PostDetailsHeaderComponent = ({ userName }: any) => {
     let { id } = useParams();
     return (
         <ComponentWrapper>
             <BtnBack to={`/user/${id}`} className="fas fa-arrow-left"><span>Back</span></BtnBack>
-            <Title >  User Name </Title>
+            <Title >  {userName.name} </Title>
             <div></div>
         </ComponentWrapper>
     )
--- src/Containers/PostDetailsContainer/PostDetailsHeaderContainer.tsx
+++ src/Containers/PostDetailsContainer/PostDetailsHeaderContainer.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
 import { PostDetailsHeaderComponent } from '../../Components/PostDetailsComponent/PostDetailsHeaderComponent'
-export const PostDetailsHeaderContainer = () => {
+import { fetchUser } from '../../Actions/UserDetailsActions'
 
+export const PostDetailsHeaderContainer = () => {
+    const dispatch = useDispatch();
+    let { id } = useParams();//  user id, which is able in location
+    useEffect(() => {
+        async function fetchData() {
+            await dispatch(fetchUser(id))
+        }
+        fetchData();
+    })
+
+    const User = useSelector((state: any) => state.user.user)
     return (
-        <PostDetailsHeaderComponent />
+        <PostDetailsHeaderComponent userName={User} />
     )
 }
