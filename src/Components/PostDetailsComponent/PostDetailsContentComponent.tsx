9d262af8b58be96ab62274f71069613ed404fce1
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-06-11 23:43:23 +0200

    connect fetchUserPost with component

--- src/Components/PostDetailsComponent/PostDetailsContentComponent.tsx
+++ src/Components/PostDetailsComponent/PostDetailsContentComponent.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { ComponentWrapper } from '../../Helpers/Styles/PostDetailsComponent/PostDetailsContentComponent'
-export const PostDetailsContentComponent = () => {
+export const PostDetailsContentComponent = ({ detailsData }: any) => {
+    const { title, body } = detailsData
     return (
         <ComponentWrapper>
-            <h2 >title</h2>
-            <p > body</p>
+            <h2 >{title}</h2>
+            <p> {body}</p>
         </ComponentWrapper>
     )
 }
--- src/Containers/PostDetailsContainer/PostDetailsContentContainer.tsx
+++ src/Containers/PostDetailsContainer/PostDetailsContentContainer.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { UseParams } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
 import { PostDetailsContentComponent } from '../../Components/PostDetailsComponent/PostDetailsContentComponent'
+import { fetchUserPost } from '../../Actions/PostDetailsActions'
+import { useParams } from 'react-router'
+
 export const PostDetailsContentContainer = () => {
+    const { type } = useParams()
+    const dispatch = useDispatch();
+    useEffect(() => {
+        async function fetchData() {
+            await dispatch(fetchUserPost(type))
+        }
+        fetchData()
+    })
+
+    const PostData = useSelector((state: any) => state.postDetails.post)
+
     return (
-        <PostDetailsContentComponent />
+        <PostDetailsContentComponent detailsData={PostData} />
     )
 }
