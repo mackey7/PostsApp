b78e4d0637b87e102cf7ef4081f8ce130886ef3f
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-06-11 22:45:16 +0200

    create PostDetailsContentContainer and connect with PostDetailsPage

--- src/Pages/PostDetailsPage.tsx
+++ src/Pages/PostDetailsPage.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { PostDetailsHeaderContainer } from '../Containers/PostDetailsContainer/PostDetailsHeaderContainer'
+import { PostDetailsContentContainer } from '../Containers/PostDetailsContainer/PostDetailsContentContainer'
 export const PostDetailsPage = () => {
     return (
         <React.Fragment>
             <PostDetailsHeaderContainer />
+            <PostDetailsContentContainer />
         </React.Fragment>
     )
 }
