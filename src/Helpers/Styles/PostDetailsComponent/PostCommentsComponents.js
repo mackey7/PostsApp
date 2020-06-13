940f0c3a0e2f483b818b301b2335d3c595cc4cd1
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-06-13 09:57:58 +0200

    create sstyle for SwitchesContainer

--- src/Helpers/Styles/PostDetailsComponent/PostCommentsComponents.js
+++ src/Helpers/Styles/PostDetailsComponent/PostCommentsComponents.js
@@ -2,11 +2,13 @@ import Styled from 'styled-components'
 
 
 export const ComponentWrapper = Styled.section`
-
+width:80%;
+margin:0 auto;
 `
 
 export const SwitchesContainer = Styled.div`
-
+display:flex;
+justify-content: space-between;
 `
 
 export const Btn = Styled.input`
