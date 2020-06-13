cafb3898f30ea3697383e6c3443d17c2f6715e37
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-06-13 09:20:48 +0200

    map props

--- src/Components/PostDetailsComponent/PostCommentsComponents.tsx
+++ src/Components/PostDetailsComponent/PostCommentsComponents.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
 
-export const PostCommentsComponents = () => {
+export const PostCommentsComponents = ({ comments }) => {
     return (
-        <div>PostCommentsComponents</div>
+        comments ? comments.map((data: any) => {
+
+            return (
+                < SingleComment key={data.id} data={data} />
+            )
+        }) : <div>Loading...</div>
     )
 }
