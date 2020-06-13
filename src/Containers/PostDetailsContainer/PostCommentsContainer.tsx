e0435b864400dabc8121e4c548d6a3c6dfc2145e
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-06-13 08:57:56 +0200

    connect  with api in container

--- src/Containers/PostDetailsContainer/PostCommentsContainer.tsx
+++ src/Containers/PostDetailsContainer/PostCommentsContainer.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState, } from 'react'
 import { PostCommentsComponents } from '../../Components/PostDetailsComponent/PostCommentsComponents'
+import { useDispatch, useSelector } from 'react-redux'
+import { useParams } from "react-router-dom";
+import { fetchPostComments } from '../../Actions/PostDetailsActions'
+
 export const PostCommentsContainer = () => {
+    let { id } = useParams();//  user id, which is able in location
+    const dispatch = useDispatch()
+    useEffect(() => {
+        async function fetchData() {
+            await dispatch(fetchPostComments(id))
+        }
+        fetchData()
+    }, [])
+    const Comments = useSelector((state: any) => state.comments.comments)
     return (
-        <PostCommentsComponents />
+        <PostCommentsComponents comments={Comments} />
     )
 }
