c71ca879a38c874e9ff897e2368c75d3c241cdf8
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-06-13 09:41:24 +0200

    add styles to ComponentWrapper and CommentHeader tag

--- src/Helpers/Styles/PostDetailsComponent/SingleComment.js
+++ src/Helpers/Styles/PostDetailsComponent/SingleComment.js
@@ -2,9 +2,13 @@ import Styled from 'styled-components'
 const BLACK = "black"
 
 export const ComponentWrapper = Styled.section`
-
+border:2px solid ${BLACK};
+padding:20px;
+margin:20px 20px;
 `
 
 export const CommentHeader = Styled.div`
-
+display:flex;
+justify-content:space-between;
+align-items:center;
 `
