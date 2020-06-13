4268581c7468aaade6ffd6069e67473f5ad0872f
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-06-13 09:59:12 +0200

    create style for Btn

--- src/Helpers/Styles/PostDetailsComponent/PostCommentsComponents.js
+++ src/Helpers/Styles/PostDetailsComponent/PostCommentsComponents.js
@@ -1,5 +1,6 @@
 import Styled from 'styled-components'
 
+const BLUE = "blue"
 
 export const ComponentWrapper = Styled.section`
 width:80%;
@@ -12,5 +13,8 @@ justify-content: space-between;
 `
 
 export const Btn = Styled.input`
-
+border:0;
+background:none;
+color:${BLUE}
+text-decoration:underline;
 `
