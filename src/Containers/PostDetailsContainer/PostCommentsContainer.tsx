6aca41da0ef5e36bf4b4ccb74e5283b24c18bec8
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-06-13 08:16:03 +0200

    create PostCommentsContainer and switch with page

---
+++ src/Containers/PostDetailsContainer/PostCommentsContainer.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const PostCommentsContainer = () => {
+    return (
+        <div>PostCommentsContainer</div>
+    )
+}
--- src/Pages/PostDetailsPage.tsx
+++ src/Pages/PostDetailsPage.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { PostDetailsHeaderContainer } from '../Containers/PostDetailsContainer/PostDetailsHeaderContainer'
 import { PostDetailsContentContainer } from '../Containers/PostDetailsContainer/PostDetailsContentContainer'
+import { PostCommentsContainer } from '../Containers/PostDetailsContainer/PostCommentsContainer'
 export const PostDetailsPage = () => {
     return (
         <React.Fragment>
             <PostDetailsHeaderContainer />
             <PostDetailsContentContainer />
+            <PostCommentsContainer />
         </React.Fragment>
     )
 }
